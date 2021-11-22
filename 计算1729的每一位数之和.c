432d40f5d064a82ca6d7b6a2b8dc2b50264302ec
1LYC <92176579+1LYC@users.noreply.github.com>
2021-11-22 23:10:04 +0800

    Update and rename 2021.11.4（1）.txt to 计算1729的每一位数之和.c

---
+++ 计算1729的每一位数之和.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int Sum(int i)
+{
+	if (i > 9)
+	{
+		return Sum(i / 10) + i % 10;
+	}
+	else
+	{
+		return i;
+	}
+}
+int main()
+{
+	int i = 1729;
+	int n = Sum(i);
+	printf("%d", n);
+	return 0;
+}
