4096b8d6a3d9c6087243015fb1068bbb3a87d06d
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-04 22:18:41 +0800

    Add files via upload

---
+++ 输入两个数求这两个数的最大公约数.c
@@ -0,0 +1,49 @@
+//#define _CRT_SECURE_NO_WARNINGS
+////#include<stdio.h>
+////
+////int my_max(int max, int i,int n)
+////{
+////	while (max)
+////	{
+////		if ((i % max == 0) && (n % max == 0))
+////		{
+////			return max;
+////		}
+////		max--;
+////	} 
+////}
+////
+////int main()
+////{
+////	int i = 0;
+////	int n = 0;
+////	scanf("%d %d", &i, &n);
+////	int max = n;
+////	if (i < n)
+////	{
+////		max = i;
+////  }
+////	int ret = my_max(max,i,n);
+////	printf("%d", ret);
+////	return 0;
+////}
+//#include<stdio.h>
+//void my_max(int i, int n)
+//{
+//	
+//	int m = 0;
+//	while (m=i%n)
+//	{
+//		i = n;
+//		n = m;
+//	}
+//	printf("%d", n);
+//}
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	scanf("%d %d", &i, &n);		
+//	my_max(i,n);
+//	return 0;
+//}
