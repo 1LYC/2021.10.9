3a57057987365c76e695d3b406dfdb34a39cc8b5
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-13 09:32:25 +0800

    Update 交换两个int变量的值，不能使用第三个变量.c

--- 交换两个int变量的值，不能使用第三个变量.c
+++ 交换两个int变量的值，不能使用第三个变量.c
@@ -1,13 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+//a^a=0
+//a^0=a
 int main()
 {
 	int a = 10;
 	int b = 20;
-	printf("a=%d b=%d", a, b);
+	printf("交换前：a=%d b=%d", a, b);
 	a = a ^ b;
-	b = a ^ b;
-	a = a ^ b;
-	printf(" a=%d b=%d", a, b);
+	b = a ^ b;//a^b^b=a
+	a = a ^ b;//a^b^a=b
+	printf("交换后：a=%d b=%d", a, b);
 	return 0;
 }
