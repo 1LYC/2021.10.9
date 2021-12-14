fd15117c1425117e3a42c8197452bca163055845
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-14 14:43:19 +0800

    Update 编写函数不允许创建临时变量，求字符串长度.c

--- 编写函数不允许创建临时变量，求字符串长度.c
+++ 编写函数不允许创建临时变量，求字符串长度.c
@@ -1,18 +1,20 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int my_strlen(char* pc)
-//{
-//	char* pt = pc;
-//	while (*pc)
-//	{
-//		pc++;
-//	}
-//	return pc - pt;
-//}
-//int main()
-//{
-//	char arr[] = { "abcdef" };
-//	int ret=my_strlen(arr);
-//	printf("%d", ret);
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//相当于模拟实现strlen
+int my_strlen(char* pc)
+{
+	char* pt = pc;
+	while (*pc)
+	{
+		pc++;
+	}
+	return pc - pt;//指针-指针
+}
+
+int main()
+{
+	char arr[] = { "abcdef" };
+	int ret=my_strlen(arr);
+	printf("%d", ret);
+	return 0;
+}
