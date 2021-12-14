75b100b1aac8f803fa1ca05e96acca1cfdd96883
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-14 15:24:27 +0800

    Update 编写函数不允许创建临时变量，求字符串长度.c

--- 编写函数不允许创建临时变量，求字符串长度.c
+++ 编写函数不允许创建临时变量，求字符串长度.c
@@ -1,20 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS
+//版本1：指针-指针
+//#include<stdio.h>
+////相当于模拟实现strlen
+//int my_strlen(char* pc)
+//{
+//	char* pt = pc;
+//	while (*pc)
+//	{
+//		pc++;
+//	}
+//	return pc - pt;
+//}
+//int main()
+//{
+//	char arr[] = { "abcdef" };
+//	int ret=my_strlen(arr);
+//	printf("%d", ret);
+//	return 0;
+//}
+// 
+// 
+//版本2：计数器
+//#include<stdio.h>
+//int my_strlen(char* pc)
+//{
+//	int count = 0;
+//	while (*pc++)
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = { "abcdef" };
+//	int ret = my_strlen(arr);
+//	printf("%d", ret);
+//	return 0;
+//}
+//版本3：递归
 #include<stdio.h>
-//相当于模拟实现strlen
-int my_strlen(char* pc)
+#include<assert.h>
+int my_strlen(const char* pr)
 {
-	char* pt = pc;
-	while (*pc)
+	assert(pr);
+	if (*pr)
 	{
-		pc++;
+		return 1+my_strlen(pr + 1);
 	}
-	return pc - pt;//指针-指针
+	return 0;
 }
-
 int main()
 {
 	char arr[] = { "abcdef" };
-	int ret=my_strlen(arr);
+	int ret = my_strlen(arr);
 	printf("%d", ret);
 	return 0;
 }
