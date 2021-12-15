eae11423adce6d876d3247a821d2e22dc5d4b730
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-15 10:54:58 +0800

    Update 求10个数中的最大值.c

--- 求10个数中的最大值.c
+++ 求10个数中的最大值.c
@@ -1,17 +1,19 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int main()
-//{
-//	int i = 0;
-//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int max = arr[0];
-//	for (i = 0;i < 10;i++)
-//	{
-//		if (arr[i] > max)
-//		{
-//			max = arr[i];
-//		}
-//	}
-//	printf("%d", max);
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//也有另一种可能 int arr[10]={-1,-2,-3,-4,-5,-6,-7,-8,-9,-10}
+	int max = arr[0];//所以这里不能写成int max = 0,
+	//应该将这10个数中的某一个与其他数进行比较
+	for (i = 0;i < 10;i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	printf("%d", max);
+	return 0;
+}
