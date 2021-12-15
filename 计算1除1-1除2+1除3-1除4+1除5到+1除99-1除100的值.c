970af395b00c67919536759bb6a06749f04a4a4e
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-15 10:46:00 +0800

    Update 计算1除1-1除2+1除3-1除4+1除5到+1除99-1除100的值.c

--- 计算1除1-1除2+1除3-1除4+1除5到+1除99-1除100的值.c
+++ 计算1除1-1除2+1除3-1除4+1除5到+1除99-1除100的值.c
@@ -1,22 +1,22 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int main()
-//{
-//	int i = 0;
-//	double ret = 1.0;
-//	for (i = 2;i <= 100;i++)
-//	{
-//		int flag = 1.0;
-//		if (i % 2 != 0)
-//		{
-//			ret += flag * 1.0 / i;
-//		}
-//		else
-//		{
-//			flag = -1;
-//			ret += flag * 1.0 / i;
-//		}
-//	}
-//	printf("%lf", ret);
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	double ret = 1.0;
+	for (i = 2;i <= 100;i++)
+	{
+		int flag = 1.0;//用这个标记实现正负之间的交替
+		if (i % 2 != 0)
+		{
+			ret += flag * 1.0 / i;
+		}
+		else
+		{
+			flag = -1;
+			ret += flag * 1.0 / i;
+		}
+	}
+	printf("%lf", ret);
+	return 0;
+}
