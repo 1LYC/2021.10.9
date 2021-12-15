616c0913f636a645c39502f80fae1c1cae4af6c0
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-15 11:23:19 +0800

    Update 递归实现n的k次方.c

--- 递归实现n的k次方.c
+++ 递归实现n的k次方.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 double Pow(int n, int k)
 {
+	//这里思考，k的正负，
 	if (k == 0)
 	{
 		return 1;
@@ -12,14 +13,15 @@ double Pow(int n, int k)
 	}
 	else
 	{
-		return 1.0 / (Pow(n, -k));
+		//将2的-3次方，变成1/(2的3)次方再进行递归实现
+		return 1.0 / (Pow(n, -k));//这里设计的比较巧妙，
 	}
 }
 int main()
 {
 	int n = 0;
 	int k = 0;
-	scanf("%d %d", &n,&k);
+	scanf("%d %d", &n, &k);
 	double ret = Pow(n, k);
 	printf("%lf", ret);
 	return 0;
