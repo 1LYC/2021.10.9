c4f713dc88cd9802de5d4fb938bb825fe7ce122d
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-18 21:23:54 +0800

    Update 实现一个加，减，乘，除计算器.c

--- 实现一个加，减，乘，除计算器.c
+++ 实现一个加，减，乘，除计算器.c
@@ -10,7 +10,7 @@ void menu()
 }
 int add(int x, int y)
 {
-	return x+y;
+	return x + y;
 }
 int sub(int x, int y)
 {
@@ -24,80 +24,82 @@ int div(int x, int y)
 {
 	return x / y;
 }
+//int main()
+//{
+//	int input = 0;
+//	do
+//	{
+//		//感觉就是将一堆函数放入了数组
+//		menu();
+//		int (*parr[5])(int, int) = { NULL,add,sub,nul,div };//应用了函数数组指针，是数组吧
+//		int x = 0;
+//		int y = 0;
+//		printf("请输入你的选择：>");
+//		scanf("%d", &input);
+//		if (input >= 1 && input <= 4)
+//		{
+//			printf("请输入两个操作数：>");
+//			scanf("%d %d", &x, &y);
+//			int ret = (parr[input])(x, y);//调用了函数数组
+//			printf("%d\n", ret);
+//		}
+//		else if (input == 0)
+//		{
+//			printf("退出程序\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("选择错误\n");
+//		}
+//	} while (input);
+//	return 0;
+//}
+
+//应用了函数回调机制，使用了函数指针
+void calc(int (*pt)(int x,int y))
+{
+	printf("请输入两个操作数：>");
+	int x = 0;
+	int y = 0;
+	scanf("%d %d", &x, &y);
+	int ret=(*pt)(x,y);
+	printf("%d\n", ret);
+}
 int main()
 {
 	int input = 0;
 	do
 	{
 		menu();
-		int (*parr[5])(int, int) = { NULL,add,sub,nul,div };//应用了函数数组指针
-		int x = 0;
-		int y = 0;
 		printf("请输入你的选择：>");
 		scanf("%d", &input);
-		if (input >= 1 && input <= 4)
+		switch (input)
 		{
-			printf("请输入两个操作数：>");
-			scanf("%d %d", &x, &y);
-			int ret = (parr[input])(x, y);//调用了函数数组
-			printf("%d\n", ret);
-		}
-		else if(input == 0)
-		{
-			printf("退出程序\n");
+		case 1:
+			calc(add);
 			break;
-		}
-		else
-		{
-			printf("选择错误\n");
+		case 2:
+			calc(sub);
+			break;
+		case 3:
+			calc(nul);
+			break;
+		case 4:
+			calc(div);
+			break;
+		default:
+			if (input == 0)
+			{
+				printf("退出程序\n");
+			}
+			else
+			{
+				printf("选择错误\n");
+			}
+			break;
 		}
 	} while (input);
 	return 0;
 }
 
-//应用了函数回调机制，使用了函数指针
-//void calc(int (*pt)(int x,int y))
-//{
-//	printf("请输入两个操作数：>");
-//	int x = 0;
-//	int y = 0;
-//	scanf("%d %d", &x, &y);
-//	int ret=(*pt)(x,y);
-//	printf("%d\n", ret);
-//}
-//int main()
-//{
-//	int input = 0;
-//	do
-//	{
-//		menu();
-//		printf("请输入你的选择：>");
-//		scanf("%d", &input);
-//		switch (input)
-//		{
-//		case 1:
-//			calc(add);
-//			break;
-//		case 2:
-//			calc(sub);
-//			break;
-//		case 3:
-//			calc(nul);
-//			break;
-//		case 4:
-//			calc(div);
-//			break;
-//		default:
-//			if (input == 0)
-//			{
-//				printf("退出程序\n");
-//			}
-//			else
-//			{
-//				printf("选择错误\n");
-//			}
-//			break;
-//		}
-//	} while (input);
-//	return 0;
-//}
