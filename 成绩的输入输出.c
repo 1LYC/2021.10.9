7b3c82fc964090f7d53373947847c8e319f051d8
1LYC <92176579+1LYC@users.noreply.github.com>
2021-12-27 18:28:04 +0800

    Update and rename 2021.10.18（1）.txt to 成绩的输入输出.c

---
+++ 成绩的输入输出.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("score1=%d,score2=%d,score3=%d",a,b,c);
+    return 0;
+}
