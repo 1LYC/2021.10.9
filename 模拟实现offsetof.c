e9eb3b4857129c38c5f1e3288781cb9df1edd5e9
1LYC <92176579+1LYC@users.noreply.github.com>
2022-02-23 10:05:32 +0800

    Rename 写一个宏计算结构体中某变量相对于首地址的偏移量.c to 模拟实现offsetof.c

--- 模拟实现offsetof.c
+++ 模拟实现offsetof.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #define OFFSETOF(i,j) (int)&(((i*)0)->j)
-//�൱����0��ַ������һ���ṹ��ָ�룬��ָ������ṹ�壬���׵�ַ0��ʼ����
+//相当于在0地址处放了一个结构体指针，将指向这个结构体，从首地址0开始访问
 struct MyStruct
 {
 	char a;
